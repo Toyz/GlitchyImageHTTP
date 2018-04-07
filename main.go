3bafa954f254061f03591db81b303eaadd3a9c12
Neon <toyzz@live.com>
2018-04-06 19:59:58 -0700

    first push

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bytes"
+	"crypto/md5"
+	"encoding/base64"
+	"fmt"
+	"image"
+	"image/png"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"text/template"
+	"time"
+
+	glitch "github.com/sugoiuguu/go-glitch"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("./tmpls/index.html")
+		t.Execute(w, token)
+	}
+}
+
+// Exampel expression:
+// 	128 & (c + 255) : (s ^ (c ^ 255)) + 25
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		r.ParseMultipartForm(32 << 20)
+		file, _, err := r.FormFile("uploadfile")
+		expression := r.FormValue("expression")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		img, _, _ := image.Decode(file)
+
+		buff := new(bytes.Buffer)
+
+		expr, _ := glitch.CompileExpression(expression)
+		out, _ := glitch.JumblePixels(expr, img)
+		png.Encode(buff, out)
+
+		imgBase64Str := base64.StdEncoding.EncodeToString(buff.Bytes())
+
+		t, _ := template.ParseFiles("./tmpls/img.html")
+		t.Execute(w, imgBase64Str)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/upload", upload)
+
+	log.Fatal(http.ListenAndServe(getEnv("LISTEN", ":8080"), nil))
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}

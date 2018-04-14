f9d05b123d46dbc691b80e767f9dd83eaf4f7ece
Neon <toyzz@live.com>
2018-04-14 14:13:55 -0700

    removed proxy for now

--- main.go
+++ main.go
@@ -168,7 +168,7 @@ func ViewImage(ctx iris.Context) {
 		log.Println(err)
 	}
 
-	image.FullPath = fmt.Sprintf("%s://%s/%s/%s", "https", ctx.Host(), "img", image.FileName)
+	//image.FullPath = fmt.Sprintf("%s://%s/%s/%s", "https", ctx.Host(), "img", image.FileName)
 
 	data := ctx.GetViewData()["Header"].(core.HeaderMetaData)
 	header := core.Render.Header(fmt.Sprintf("%s - View Image %s", data.Title, image.ID), image.FullPath, data.Desc, image.ID)
@@ -217,18 +217,20 @@ func main() {
 	app.Get("/", Index)
 	app.Post("/upload", Upload)
 	app.Get("/{image:string}", ViewImage)
-	switch core.GetSaveMode() {
-	case "aws":
-		app.Get("/img/{file:string}", func(ctx iris.Context) {
-			data := saveMode.Read(ctx.Params().Get("file"))
+	/*
+		switch core.GetSaveMode() {
+		case "aws":
+			app.Get("/img/{file:string}", func(ctx iris.Context) {
+				data := saveMode.Read(ctx.Params().Get("file"))
 
-			ctx.Header("content-type", "image/png")
-			ctx.Write(data)
-		})
-		break
-	case "fs":
-		app.StaticWeb("/img", "./assets/uploads")
-	}
+				ctx.Header("content-type", "image/png")
+				ctx.Write(data)
+			})
+			break
+		case "fs":
+			app.StaticWeb("/img", "./assets/uploads")
+		}
+	*/
 	app.StaticWeb("/static", "./assets/public")
 
 	app.Build()

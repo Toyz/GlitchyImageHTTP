69682791f5e4a14aad7ea9d2fc1418f546be33f9
Neon <toyzz@live.com>
2018-04-17 20:34:57 -0700

    fiileUpload fix

--- assets/public/js/fileUpload.js
+++ assets/public/js/fileUpload.js
@@ -1,11 +1,13 @@
-$('.uploadFile').click(function() {
-  $('.uploadFile').bind('change', function () {
-    var file = $(".uploadFile")[0].files[0];
-    if (/^\s*$/.test(file.name)) {
-      $(".fileName").text("...");
-    }
-    else {
-      $(".fileName").text(file.name);
-    }
+$(document).ready(function() {
+  $('.uploadFile').click(function() {
+    $('.uploadFile').bind('change', function () {
+      var file = $('.uploadFile')[0].files[0];
+      if (/^\s*$/.test(file.name)) {
+        $('.fileName').text("...");
+      }
+      else {
+        $('.fileName').text(file.name);
+      }
+    });
   });
 });

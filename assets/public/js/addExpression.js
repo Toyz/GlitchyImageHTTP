8964e1f9321eab312050cf8491256d9e4e282525
Neon <toyzz@live.com>
2018-04-17 19:24:48 -0700

    Minor template fixes

--- assets/public/js/addExpression.js
+++ assets/public/js/addExpression.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
   var counter = 1
   var limit = 5
-  $('#addExpr').click(function() {
+  $('.addExpr').click(function() {
     if (counter == limit) {
       alert("You have reached the limit of adding " + counter + " expressions");
     }
     else {
       counter++;
-      $('#expressionList').append( '<div class="expressionForm" id=' + (counter + 1) + '><label for="expression"><span>>> expression.( " </span></label><input type="text" name="expression" value="" class="expression" /><span> " )</span></div>' );
+      $('.expressionList').append( '<div class="expressionForm" id=' + (counter + 1) + '><label for="expression"><span>>> expression.( " </span></label><input type="text" name="expression" value="" class="expression" /><span> " )</span></div>' );
       if (counter > 1) {
-        $('#removeExpr').addClass('unhide');
+        $('.removeExpr').addClass('unhide');
       }
     }
   })
-  $('#removeExpr').click(function() {
+  $('.removeExpr').click(function() {
     $('.expressionForm#'+counter).remove();
     counter--;
     if (counter == 1) {
-      $('#removeExpr').removeClass('unhide');
+      $('.removeExpr').removeClass('unhide');
     }
   })
 })
